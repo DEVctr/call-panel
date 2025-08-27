a0084410d69d400e29c2fccd030fc48d257a23c7
DEVctr <vferreira487@gmail.com>
2025-08-27 01:41:56 -0300

    ✨ feature: add Call DTO model

---
+++ src/main/java/com/devctr/call_panel/model/dto/CallDTO.java
@@ -0,0 +1,12 @@
+package com.devctr.call_panel.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CallDTO {
+    private String name;
+    private String counter;
+    private String status;
+}
