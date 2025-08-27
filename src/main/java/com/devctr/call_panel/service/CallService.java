985acc7f2b945b1e4f95fa5ff7f450a3e279184c
DEVctr <vferreira487@gmail.com>
2025-08-27 01:42:45 -0300

    ✨ feature: add call service

---
+++ src/main/java/com/devctr/call_panel/service/CallService.java
@@ -0,0 +1,20 @@
+package com.devctr.call_panel.service;
+
+import org.springframework.stereotype.Service;
+
+import com.devctr.call_panel.model.dto.CallDTO;
+import com.devctr.call_panel.model.entity.Call;
+import com.devctr.call_panel.repository.CallRepositoy;
+
+@Service
+public class CallService {
+    private final CallRepositoy callRepository;
+
+    public CallService(CallRepositoy callRepository) {
+        this.callRepository = callRepository;
+    }
+    
+    public Call save(CallDTO CallDTO) {
+        return callRepository.save(new Call(CallDTO));
+    }
+}
