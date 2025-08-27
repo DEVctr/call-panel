6ffdab06a934f966648c4fd12b26fe864e43cce5
DEVctr <vferreira487@gmail.com>
2025-08-27 01:42:57 -0300

    ✨ feature: add call controller

---
+++ src/main/java/com/devctr/call_panel/controller/CallController.java
@@ -0,0 +1,23 @@
+package com.devctr.call_panel.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devctr.call_panel.model.dto.CallDTO;
+import com.devctr.call_panel.model.entity.Call;
+import com.devctr.call_panel.service.CallService;
+
+@RestController
+@RequestMapping("/call")
+public class CallController {
+    private final CallService callService;
+
+    public CallController(CallService callService) {
+        this.callService = callService;
+    }
+
+    public Call createCall(@RequestBody CallDTO callDTO) {
+        return callService.save(callDTO);
+    }
+}
