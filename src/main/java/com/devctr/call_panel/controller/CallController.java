b3ef1929f18f0a9c8094c7cbde08b64c5e5fe8dd
DEVctr <vferreira487@gmail.com>
2025-08-27 03:38:09 -0300

    ✨ feature: add web socket configuration file

--- src/main/java/com/devctr/call_panel/controller/CallController.java
+++ src/main/java/com/devctr/call_panel/controller/CallController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
 import com.devctr.call_panel.model.dto.CallDTO;
 import com.devctr.call_panel.model.entity.Call;
 import com.devctr.call_panel.service.CallService;
@@ -13,13 +15,19 @@ import com.devctr.call_panel.service.CallService;
 @RequestMapping("/call")
 public class CallController {
     private final CallService callService;
+    private final SimpMessagingTemplate simpMessageTemplate;
 
-    public CallController(CallService callService) {
+    public CallController(CallService callService, SimpMessagingTemplate simpMessageTemplate) {
         this.callService = callService;
+        this.simpMessageTemplate = simpMessageTemplate;
     }
 
     @PostMapping
     public Call createCall(@RequestBody CallDTO callDTO) {
-        return callService.save(callDTO);
+        Call newCall = callService.save(callDTO);
+
+        simpMessageTemplate.convertAndSend("/topic/calls", newCall);
+
+        return newCall;
     }
 }
