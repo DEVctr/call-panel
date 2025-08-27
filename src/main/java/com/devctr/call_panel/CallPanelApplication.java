cafb90ba1e1c8b9cfc49ff0595794c0e0c381baf
DEVctr <vferreira487@gmail.com>
2025-08-27 00:08:49 -0300

    initial spring boot project

---
+++ src/main/java/com/devctr/call_panel/CallPanelApplication.java
@@ -0,0 +1,13 @@
+package com.devctr.call_panel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallPanelApplication.class, args);
+	}
+
+}
