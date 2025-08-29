d1ad4da16cf7514ec46d17d3080bd4c21c02c26a
DEVctr <vferreira487@gmail.com>
2025-08-29 10:48:41 -0300

    ✨ feature: adds Attendant entity and Login Request DTO

---
+++ src/main/java/com/devctr/call_panel/model/entity/Attendant.java
@@ -0,0 +1,26 @@
+package com.devctr.call_panel.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Attendant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String password;
+    private Integer counter;
+}
