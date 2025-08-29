0d4ad60c32bfb4b72f8719727d58de95c249d2ca
DEVctr <vferreira487@gmail.com>
2025-08-29 10:47:04 -0300

    ♻️  refactor: changes Call entity and CallDTO

--- src/main/java/com/devctr/call_panel/model/dto/CallDTO.java
+++ src/main/java/com/devctr/call_panel/model/dto/CallDTO.java
@@ -2,13 +2,19 @@ package com.devctr.call_panel.model.dto;
 
 import com.devctr.call_panel.model.enums.CallStatus;
 
+import jakarta.annotation.Nullable;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CallDTO {
+    @Nullable
+    private Integer code;
+    @Nullable
     private String name;
-    private String counter;
+    
+    @Nullable
+    private Integer counter;
     private CallStatus status;
 }
--- src/main/java/com/devctr/call_panel/model/entity/Call.java
+++ src/main/java/com/devctr/call_panel/model/entity/Call.java
@@ -5,27 +5,34 @@ import java.util.Date;
 import com.devctr.call_panel.model.dto.CallDTO;
 import com.devctr.call_panel.model.enums.CallStatus;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Call {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Nullable
     private String name;
-    private String counter;
+
+    private Integer code;
+    private Integer counter;
     private CallStatus status;
     private Date creationDate;
 
     public Call(CallDTO callDTO) {
+        this.code = callDTO.getCode();
         this.name = callDTO.getName();
         this.counter = callDTO.getCounter();
         this.status = callDTO.getStatus();
